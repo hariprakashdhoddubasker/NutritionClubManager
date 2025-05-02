cb6a79c4dea1591077ca48d16fb1ccd693589d4c
Hari Basker <hari.basker@necsws.com>
2025-05-02 18:24:51 +0530

    Init: Empty MVC Project

---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NutritionClubManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
